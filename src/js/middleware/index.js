e3777d84b09ebe9cb003a00d34a47d3fee722ecd
demitycho <tanweihao94@gmail.com>
2020-02-22 03:46:38 +0800

    Refactor middleware

--- src/js/middleware/index.js
+++ src/js/middleware/index.js
@@ -1,26 +1,4 @@
-import { ADD_TODO, MOVE_OBJECT } from "../constants/action-types";
-import store from '../store/index.js';
-
-const forbiddenWords = ["spam", "money"];
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-
-const id2List = {
-  droppable: 'todo',
-  droppable2: 'inProgress',
-  droppable3: 'done'
-};
-
-const getList = (state, id) => {
-  return state[id2List[id]];
-}
-
-export function forbiddenWordsMiddleware({ dispatch }) {
+export function middleware({ dispatch }) {
   return function(next) {
     return function(action) {
       // do your stuff
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,13 +1,13 @@
 // src/js/store/index.js
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
-import { forbiddenWordsMiddleware } from "../middleware";
+import { middleware } from "../middleware";
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware))
+  storeEnhancers(applyMiddleware(middleware))
 );
 
 export default store;

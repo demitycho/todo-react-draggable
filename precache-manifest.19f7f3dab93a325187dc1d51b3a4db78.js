36f5a890e22a897fa5868a9163f278364df635ad
demitycho <tanweihao94@gmail.com>
2020-02-22 16:19:17 +0800

    Updates

---
+++ precache-manifest.19f7f3dab93a325187dc1d51b3a4db78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bcf9b5175b427b18ff481031db9f31",
+    "url": "/todo-react-draggable/index.html"
+  },
+  {
+    "revision": "bce88ad66fb589d2a10a",
+    "url": "/todo-react-draggable/static/css/main.bad632ae.chunk.css"
+  },
+  {
+    "revision": "10db391259909aa96a71",
+    "url": "/todo-react-draggable/static/js/2.c826ed66.chunk.js"
+  },
+  {
+    "revision": "bce88ad66fb589d2a10a",
+    "url": "/todo-react-draggable/static/js/main.10d0e023.chunk.js"
+  },
+  {
+    "revision": "13474e08533a9a252747",
+    "url": "/todo-react-draggable/static/js/runtime-main.bbd5dbb3.js"
+  }
+]);
